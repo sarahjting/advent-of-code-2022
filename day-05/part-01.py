8bdf249c4a36bd7fc3589af90cc392a61cce429f
lulu <sarah.j.ting@gmail.com>
2022-12-15 01:35:46 +0900

    add day 5

---
+++ day-05/part-01.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+def parseInput(file):
+    raw = []
+    for line in file:
+        if (line == '\n'):
+            break
+        else:
+            raw.append(line)
+
+    stackLegend = [x for x in raw.pop().strip('\n').split(' ') if x != '']
+    numStacks = int(stackLegend[len(stackLegend) - 1])
+    stacks = {key: deque() for key in range(1, numStacks + 1)}
+
+    for line in raw:
+        for stackIndex in range(1, numStacks + 1):
+            crateSymbol = line[(stackIndex - 1) * 4 + 1]
+            if crateSymbol != ' ':
+                stacks[stackIndex].append(crateSymbol)
+
+    return stacks
+
+with open('input') as file:
+    stacks = parseInput(file)
+
+    do_skip = True
+    for line in file:
+        numCrates, srcStack, dstStack = [int(x) for x in line.strip('\n').split(' ') if x.isdigit()]
+        for i in range(0, numCrates):
+            stacks[dstStack].appendleft(stacks[srcStack].popleft())
+
+    print(''.join([d[0] for (key, d) in stacks.items()]))
